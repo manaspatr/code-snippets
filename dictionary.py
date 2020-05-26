4c5d3971494595dad59c6530117226e1da2a9815
manaspatr <65206973+manaspatr@users.noreply.github.com>
2020-05-26 12:09:29 +0530

    created dictionary.py
    This file print deleted elements of the dictionary on the screen

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+#deleting elements from a dictionary
+#example
+my_dict ={"c":"easy","c++":"moderate","java":"tough","python":"cool"}
+#here elements can be delete by giving del command.example
+del my_dict["c"]
+#now by accessing it will give
+print(my_dict)
+#output will be {'c++':'moderate','java':'tough','python':'cool'}
