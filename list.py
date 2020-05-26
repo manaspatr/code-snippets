1e240810589a0f70404e4ff9bc62d55b6da28c7e
manaspatr <65206973+manaspatr@users.noreply.github.com>
2020-05-26 11:44:59 +0530

    created list.py
    This file print list on the screen

---
+++ list.py
@@ -0,0 +1,6 @@
+#assigning elements in list
+#we can assign elements in a square brackets with'='sign
+name =["mkp","john","johny"]
+#here "mkp","john","johny" are elements in  the list
+print(name)
+#the output will be ['mkp','john','johny']
