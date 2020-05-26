ac1ebeceb1c72c675ccdb1a9d570e212fc7bc6cc
manaspatr <65206973+manaspatr@users.noreply.github.com>
2020-05-26 12:01:22 +0530

    created tuple.py
    This file print tuple on the screen

---
+++ tuple.py
@@ -0,0 +1,11 @@
+#accessing elements from a tuple
+#elements in tuple are access by round brackets
+my_tuple =("c","c++","java","python")
+print(my_tuple)
+#output will be ('c','c++','java','python')
+#the length can be access in the tuple
+print(len(my_tuple))
+#output will be 4
+#it can have index-based access
+print(my_tuple[1])
+#output will be c++
